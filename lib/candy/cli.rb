0087d432360aa8e75df5e71896905c48627f690a
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-08-29 01:23:13 +0000

    Update cli

--- lib/candy/cli.rb
+++ lib/candy/cli.rb
@@ -50,46 +50,17 @@ class Candy::CLI
       puts ""
       category_input = gets.strip.downcase
 
-      # if category_input.to_i.between?(1, Candy::Category.all.length)
-      #   category = NYStateFairEvents::Concert.all[category_input.to_i-1]
-      #   category.category_details
-      #   puts "Click on any candy below for more information"
+      if category_input.to_i.between?(1, Candy::Category.all.length)
+        # category = Candy::Category.all[category_input.to_i-1]
+        # List out all candy in a given category
+        puts "Click on any candy below for more information"
       if category_input.to_i == 1
-        puts "1. SPANGLER Individually Wrapped Mini Peppermint Candy Canes Bulk, 500 Count"
-        # puts "$29.95"
+        puts "1. SPANGLER Individually Wrapped Mini Peppermint Candy Canes Bulk, 500 Count - $29.95"
         # puts "For more information visit: https://www.candy.com/spangler-mini-peppermint-candy-canes-bulk-500-count"
-        puts "2. Spangler Peppermint Candy Cane Jar, 60 count"
-        # puts "$29.95"
+        puts "2. Spangler Peppermint Candy Cane Jar, 60 count - $29.95"
         # puts "For more information visit: https://www.candy.com/spangler-peppermint-candy-cane-jar-60-count"
-        puts "3. Spangler Cherry Candy Canes, 12 Count, 3 Pack"
-        # puts "$9.95"
+        puts "3. Spangler Cherry Candy Canes, 12 Count, 3 Pack - $9.95"
         # puts "For more information visit: https://www.candy.com/spangler-cherry-candy-canes-12-pieces-3-count"
-        puts <<-DOC.gsub /^\s*/, ""
-          Enter a candy number to see details on that candy. Otherwise, enter one of the following prompts:
-          - Enter 'menu' to see the main menu
-          - Enter 'exit' to exit
-        DOC
-        puts ""
-      elsif category_input.to_i == 2
-        puts "1. Individually Wrapped Candy Corn 1.5 oz, 5 lb"
-        # puts "$24.95"
-        # puts "For more information visit: https://www.candy.com/candy-corn-individual-packs-5-lbs-80-count"
-        puts <<-DOC.gsub /^\s*/, ""
-          Enter a candy number to see details on that candy. Otherwise, enter one of the following prompts:
-          - Enter 'menu' to see the main menu
-          - Enter 'exit' to exit
-        DOC
-        puts ""
-      elsif category_input.to_i == 3
-        puts "1. Classic Lobster 1 oz 12 Count"
-        # puts "$29.95"
-        # puts "For more information visit: https://www.candy.com/classic-lobster-1-oz-12-count"
-        puts "2. Lobster Claw 1 oz 12 Count"
-        # puts "$29.95"
-        # puts "For more information visit: https://www.candy.com/lobster-claw-1-oz-12-count"
-        puts "3. Seacoast Whale Tail 1 oz 12 Count"
-        # puts "$29.95"
-        # puts "For more information visit: https://www.candy.com/seacoast-whale-tail-1-oz-12-count"
         puts <<-DOC.gsub /^\s*/, ""
           Enter a candy number to see details on that candy. Otherwise, enter one of the following prompts:
           - Enter 'menu' to see the main menu
