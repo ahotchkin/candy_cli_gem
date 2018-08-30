5d0a4d2f5900ee0637863f7233d69d98922979d6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-08-30 15:18:12 +0000

    Test class collaboration

--- lib/candy/candy.rb
+++ lib/candy/candy.rb
@@ -1,28 +1,28 @@
 class Candy::Candy
   attr_accessor :name, :price, :url, :category
 
-  @@all = []
+  # @@all = []
 
   def initialize(name = nil, price = nil, url = nil)
     @name = name
     @price = price
     @url = url
-    @@all << self
+    # @@all << self
   end
 
   def category
     self.category.name
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.list_all
-    @@all.each.with_index(1) do |candy, i|
-      puts "#{i}. #{candy.name} - #{candy.price}"
-    end
-  end
+  # def self.all
+  #   @@all
+  # end
+  #
+  # def self.list_all
+  #   @@all.each.with_index(1) do |candy, i|
+  #     puts "#{i}. #{candy.name} - #{candy.price}"
+  #   end
+  # end
 
 
 end
